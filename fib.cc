a92a87cadb8a89bd7ffa127bbd90ced15e0a20c2
domiryuu <dawnofmyrequiem@gmail.com>
2016-12-14 14:55:32 -0500

    Added fib.cc which prints out fib(1) -> fib(99)

---
+++ fib.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int fib(int i);
+int main(){
+	for(int i = 0; i < 100; i++){
+		std::cout << fib(i) << std::endl;
+	}
+}
+int fib(int i){
+	if(i < 1){return 0;}else{if(i==1){return 1;}}
+	return (fib(i -1)) + (fib(i-2));
+}
