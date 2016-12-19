d43cc48b5019b0f8b2a4e8b83b6c23017848d0f5
domiryuu <dawnofmyrequiem@gmail.com>
2016-12-19 13:46:01 -0500

    Fixed indent.

--- fib.cc
+++ fib.cc
@@ -6,10 +6,10 @@ int main(){
     uint64_t i = 0;
     std::cout << "Enter a number to view it's fibonacci number (index starts at 0)\n> ";
     std::cin >> i;
-	std::cout << fib(i) << std::endl;
-	return 0;
+    std::cout << fib(i) << std::endl;
+    return 0;
 }
 
 uint64_t fib(uint64_t i){
-	return 0 == i ? 0 : 1 == i ? 1 : fib(i-1)+fib(i-2);
+    return 0 == i ? 0 : 1 == i ? 1 : fib(i-1)+fib(i-2);
 }
