67debcdfc1425182acfa3f8cb77491c4b18829cc
domiryuu <dawnofmyrequiem@gmail.com>
2016-12-19 13:29:00 -0500

    Changed type to uint64_t to allow for larger values

--- fib.cc
+++ fib.cc
@@ -1,15 +1,15 @@
 #include <iostream>
 
-int fib(int i);
+uint64_t fib(uint64_t i);
 
 int main(){
-    int i = 0;
-    std::cout << "Enter a number to view it's fibonacci number (index starts at 0)\n>";
+    uint64_t i = 0;
+    std::cout << "Enter a number to view it's fibonacci number (index starts at 0)\n> ";
     std::cin >> i;
 	std::cout << fib(i) << std::endl;
 	return 0;
 }
 
-int fib(int i){
-	return 1 > i ? 0 : 1 == i ? 1 : fib(i-1)+fib(i-2);
+uint64_t fib(uint64_t i){
+	return 0 == i ? 0 : 1 == i ? 1 : fib(i-1)+fib(i-2);
 }
